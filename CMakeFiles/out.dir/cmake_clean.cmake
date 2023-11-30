0afc4a0d3fa6de1f7ac93e361e2b165dcc4beaf4
timeFade <2934079117@qq.com>
2023-12-01 00:00:20 +0800

    some info of cpp primitives

---
+++ CMakeFiles/out.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/out.dir/HelloWorld.cpp.obj"
+  "CMakeFiles/out.dir/HelloWorld.cpp.obj.d"
+  "libout.dll.a"
+  "out.exe"
+  "out.exe.manifest"
+  "out.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/out.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -3,6 +3,6 @@ using namespace std;
 
 int main()
 {
-	cout << "Hello World !!" << enl;
+	cout << "Hello World !!" << endl;
 	return 0;
 }
